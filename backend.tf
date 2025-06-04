c7c7ab35e7362885827a688588fb14b799bdfc58
ardm17 <ardm17@sky.com>
2025-06-04 10:55:40 +0100

    update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
    backend "azurerm" {
       resource_group_name = "demo-rg"
-      storage_account_name = "storage9898TP001"
+      storage_account_name = "storage9898tp001"
       container_anem = "prod-9898"
       key = "prod.terraform.tfstate"
    }
